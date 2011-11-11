16f8d5d781cb9ce087c21c1a0f90f8cf99938028
Dan MacTough <danmactough@gmail.com>
2011-11-11 00:33:01 -0500

    Initial commit

---
+++ examples/simple.js
@@ -0,0 +1,19 @@
+/*!
+ * node-opmlparser
+ * Copyright(c) 2011 Dan MacTough <danmactough@gmail.com>
+ * MIT Licensed
+ */
+
+var OpmlParser = require('../lib/opmlparser')
+  , parser
+
+parser = new OpmlParser();
+
+parser.on('meta', function (meta){
+  console.log('This OPML is entitled: "%s"', meta.title);
+});
+parser.on('feed', function (feed){
+  console.log('Got feed: "%s" <%s>', feed.title, feed.xmlUrl);
+});
+
+parser.parseFile('http://hosting.opml.org/dave/spec/subscriptionList.opml');
