4752d5e530bf23f4de17285b7d6c0b9bd3413df5
Dan MacTough <danmactough@gmail.com>
2014-02-10 06:29:14 -0500

    Update simple example.

--- examples/simple.js
+++ examples/simple.js
@@ -1,19 +1,44 @@
 /*!
  * node-opmlparser
- * Copyright(c) 2011 Dan MacTough <danmactough@gmail.com>
+ * Copyright(c) 2011-2014 Dan MacTough <danmactough@gmail.com>
  * MIT Licensed
  */
 
 var OpmlParser = require('../')
-  , parser
+  , request = require('request');
 
-parser = new OpmlParser();
+var opmlparser = new OpmlParser()
+  , counter = 0;
 
-parser.on('meta', function (meta){
-  console.log('This OPML is entitled: "%s"', meta.title);
+var req = request('http://hosting.opml.org/dave/spec/subscriptionList.opml');
+req.on('error', done);
+req.on('response', function (res) {
+  if (res.statusCode != 200) return done(new Error('Bad status code'));
+  this.pipe(opmlparser);
+})
+
+opmlparser.on('error', done);
+opmlparser.once('readable', function () {
+  console.log('This OPML is entitled: "%s"', this.meta.title);
 });
-parser.on('feed', function (feed){
-  console.log('Got feed: "%s" <%s>', feed.title, feed.xmlurl);
+opmlparser.on('readable', function() {
+  var outline;
+
+  while (outline = this.read()) {
+    if (outline['#type'] === 'feed') {
+      counter++;
+      console.log('Got feed: "%s" <%s>', outline.title, outline.xmlurl);
+    }
+  }
+});
+opmlparser.on('end', function () {
+  console.log('All done. Found %s feeds.', counter);
 });
 
-parser.parseFile('http://hosting.opml.org/dave/spec/subscriptionList.opml');
+function done (err) {
+  if (err) {
+    console.log(err, err.stack);
+    return process.exit(1);
+  }
+  process.exit();
+}
